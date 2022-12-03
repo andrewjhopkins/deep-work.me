f06fc525cc80969f79c4112e63b6ef84c6c64505
Andrew Hopkins <ahopkins0929@gmail.com>
2022-12-03 18:39:42 -0500

    remove boilerplate

--- deepwork-client/components/Settings/SettingsModal.tsx
+++ deepwork-client/components/Settings/SettingsModal.tsx
@@ -14,54 +14,10 @@ const SettingsModal = () =>  {
       <>
         {showSettings ? (
           <>
-            <div
-              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            >
-              <div className="relative w-2/3 my-6 mx-auto max-w-3xl">
-                {/*content*/}
-                <div className="border-2 shadow-lg relative flex flex-col w-full outline-none focus:outline-none bg-gray-800 rounded-lg">
-                  <div className="border-2 grid grid-cols-2">
-
-                    <h3 className="text-3xl font-semibold">
-                      Modal Title
-                    </h3>
-                    <button
-                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                      onClick={toggleShowSettings}
-                    >
-                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                        ×
-                      </span>
-                    </button>
-                  </div>
-                  {/*body*/}
-                  <div className="relative p-6 flex-auto">
-                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                      I always felt like I could do anything. That’s the main
-                      thing people are controlled by! Thoughts- their perception
-                      of themselves! They're slowed down by their perception of
-                      themselves. If you're taught you can’t do anything, you
-                      won’t do anything. I was taught I could do everything.
-                    </p>
-                  </div>
-                  {/*footer*/}
-                  <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                    <button
-                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={toggleShowSettings}
-                    >
-                      Close
-                    </button>
-                    <button
-                      className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={toggleShowSettings}
-                    >
-                      Save Changes
-                    </button>
-                  </div>
-                </div>
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+              <div className="w-2/3 h-72 shadow-lg flex flex-col w-full outline-none focus:outline-none bg-gray-800 rounded-lg grid grid-rows-4">
+                  <div className="p-6 flex-auto row-span-3 border-2"></div>
+                  <div className="row-span-1 border-2"></div>
               </div>
             </div>
             <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
