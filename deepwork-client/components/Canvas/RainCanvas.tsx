00242015cb6de21ac967bc4770d2631d64f94413
ahopkins <ahopkins0929@gmail.com>
2022-12-18 17:23:40 -0500

    get canvas working

--- deepwork-client/components/Canvas/RainCanvas.tsx
+++ deepwork-client/components/Canvas/RainCanvas.tsx
@@ -3,17 +3,22 @@ import React, { useRef, useEffect, useState } from 'react'
 const RainCanvas = () => {
     const canvasRef = useRef(null);
 
+    const [innerWidth, setInnerWidth] = useState(0);
+    const [innerHeight, setInnerHeight] = useState(0);
+
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext("2d");
+
+        setInnerWidth(window.innerWidth);
+        setInnerHeight(window.innerHeight);
+
         context.fillStyle = "#123455";
         context.fillRect(0, 0, context.canvas.width, context.canvas.height);
     }, [])
 
-    
-
     return (
-        <canvas ref={canvasRef} className="border fixed top-0" id="rain-canvas" width={window.innerWidth} height={window.innerHeight}>
+        <canvas ref={canvasRef} className="border fixed top-0 h-full w-full" id="rain-canvas">
         </canvas>
     )
 }
