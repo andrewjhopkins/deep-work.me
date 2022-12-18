a1d6292d3a1eca11bf954639a73047041e7c03db
andrewjhopkins <ahopkins0929@gmail.com>
2022-12-18 13:40:57 -0500

    update drag points

--- deepwork-client/components/TaskList/TaskListWidget.tsx
+++ deepwork-client/components/TaskList/TaskListWidget.tsx
@@ -115,74 +115,77 @@ const TaskListWidget = memo(() => {
     })
 
     return (
-        <div className="handle cursor-move h-96 border-2 border-gray-900 bg-gray-800 bg-opacity-90 rounded-lg">
-
-            <div style={{height: "15%"}} className="grid grid-cols-6">
-                <div className="flex justify-center items-center col-span-2 text-white text-1xl text-center"> 
-                    Task List 
-                </div>
-                <div className="col-span-4 grid grid-cols-12 flex justify-center items-center">
-                    {creatingTask || updatingTask ?
-                    <div onClick={creatingTask ? toggleCreatingTask : toggleUpdatingTask} className={`cursor-pointer flex justify-center items-center col-start-8 col-span-4 bg-red-900 hover:bg-red-800 text-white font-bold rounded h-2/4`}>Exit</div> :
-                    <div onClick={toggleCreatingTask} className={`cursor-pointer flex justify-center items-center col-start-8 col-span-4 bg-blue-900 hover:bg-blue-800 text-white font-bold rounded h-2/4`}>Create</div>
-                    }
-                </div>
-            </div>
-
-            {creatingTask || updatingTask ? 
-            <div style={{height: "85%"}} className="grid grid-rows-6 p-2">
-
-                <div className="row-span-2 grid grid-rows-6">
-
-                    <div className="row-span-2 text-white grid grid-cols-8">
-                        <div className="col-span-2 flex items-center">
-                            <label className="text-gray-400 ml-2">
-                                Task
-                            </label>
-                        </div>
-
-                        {!creatingTask && 
-                            <div onClick={deleteTask} className="cursor-pointer flex items-center justify-center col-start-8 col-span-1">
-                                <IconContext.Provider value={{ color: 'white', size: '20px' }}>
-                                    <AiFillDelete />
-                                </IconContext.Provider>
-                            </div>
+        <div className="border-2 border-gray-900 bg-gray-800 bg-opacity-90 rounded-lg">
+            <div className="h-2 handle cursor-move"></div>
+            <div className="h-96">
+                <div style={{height: "15%"}} className="grid grid-cols-6">
+                    <div className="flex justify-center items-center col-span-2 text-white text-1xl text-center"> 
+                        Task List 
+                    </div>
+                    <div className="col-span-4 grid grid-cols-12 flex justify-center items-center">
+                        {creatingTask || updatingTask ?
+                        <div onClick={creatingTask ? toggleCreatingTask : toggleUpdatingTask} className={`cursor-pointer flex justify-center items-center col-start-8 col-span-4 bg-red-900 hover:bg-red-800 text-white font-bold rounded h-2/4`}>Exit</div> :
+                        <div onClick={toggleCreatingTask} className={`cursor-pointer flex justify-center items-center col-start-8 col-span-4 bg-blue-900 hover:bg-blue-800 text-white font-bold rounded h-2/4`}>Create</div>
                         }
                     </div>
-
-                    <div className="row-span-3">
-                        <input onChange={(e) => updateNewTaskName(e.target as HTMLInputElement)} value={newTask.name} className="text-white bg-gray-700 appearance-none rounded w-full h-full py-2 px-4 leading-tight focus:outline-none" placeholder="Task to complete" />
-                    </div>
                 </div>
 
-                <div className="row-span-1 grid grid-cols-12">
-                    <div className="ml-3 flex items-center col-span-9 text-white">
-                        Pomodoros
-                    </div>
-                    <div className="col-span-3 grid grid-cols-3">
-                        <div onClick={decrementNewTaskPomodoros} className="cursor-pointer col-span-1 flex justify-center items-center text-white text-1xl">-</div>
-                        <div className="col-span-1 flex justify-center items-center text-white text-1xl">{newTask.pomodoros}</div>
-                        <div onClick={incrementNewTaskPomodoros} className="cursor-pointer col-span-1 flex justify-center items-center text-white text-1xl">+</div>
-                    </div>
-                </div>
+                {creatingTask || updatingTask ? 
+                <div style={{height: "85%"}} className="grid grid-rows-6 p-2">
 
-                <div className="row-span-1 flex items-center justify-center">
-                    <div onClick={creatingTask ? createNewTask : updateTask} className="cursor-pointer flex justify-center items-center col-start-8 col-span-4 bg-blue-900 hover:bg-blue-800 text-white font-bold rounded h-3/4 w-1/3">
-                        {creatingTask ? "Create" : "Update"}
+                    <div className="row-span-2 grid grid-rows-6">
+
+                        <div className="row-span-2 text-white grid grid-cols-8">
+                            <div className="col-span-2 flex items-center">
+                                <label className="text-gray-400 ml-2">
+                                    Task
+                                </label>
+                            </div>
+
+                            {!creatingTask && 
+                                <div onClick={deleteTask} className="cursor-pointer flex items-center justify-center col-start-8 col-span-1">
+                                    <IconContext.Provider value={{ color: 'white', size: '20px' }}>
+                                        <AiFillDelete />
+                                    </IconContext.Provider>
+                                </div>
+                            }
+                        </div>
+
+                        <div className="row-span-3">
+                            <input onChange={(e) => updateNewTaskName(e.target as HTMLInputElement)} value={newTask.name} className="text-white bg-gray-700 appearance-none rounded w-full h-full py-2 px-4 leading-tight focus:outline-none" placeholder="Task to complete" />
+                        </div>
+                    </div>
+
+                    <div className="row-span-1 grid grid-cols-12">
+                        <div className="ml-3 flex items-center col-span-9 text-white">
+                            Pomodoros
+                        </div>
+                        <div className="col-span-3 grid grid-cols-3">
+                            <div onClick={decrementNewTaskPomodoros} className="cursor-pointer col-span-1 flex justify-center items-center text-white text-1xl">-</div>
+                            <div className="col-span-1 flex justify-center items-center text-white text-1xl">{newTask.pomodoros}</div>
+                            <div onClick={incrementNewTaskPomodoros} className="cursor-pointer col-span-1 flex justify-center items-center text-white text-1xl">+</div>
+                        </div>
+                    </div>
+
+                    <div className="row-span-1 flex items-center justify-center">
+                        <div onClick={creatingTask ? createNewTask : updateTask} className="cursor-pointer flex justify-center items-center col-start-8 col-span-4 bg-blue-900 hover:bg-blue-800 text-white font-bold rounded h-3/4 w-1/3">
+                            {creatingTask ? "Create" : "Update"}
+                        </div>
+                    </div>
+                </div> 
+                : 
+                <div style={{height: "85%"}} className="grid grid-rows-6 p-2">
+                    <div className="mt-2 row-span-5">
+                        {tasks}
+                    </div>
+                    <div className="grid grid-cols-12">
+                        <div onClick={removeAllTasks} className="cursor-pointer flex justify-center items-center col-start-9 col-span-4 bg-red-900 hover:bg-red-800 text-white font-bold rounded h-3/4">Remove All</div>
                     </div>
                 </div>
-            </div> 
-            : 
-            <div style={{height: "85%"}} className="grid grid-rows-6 p-2">
-                <div className="mt-2 row-span-5">
-                    {tasks}
-                </div>
-                <div className="grid grid-cols-12">
-                    <div onClick={removeAllTasks} className="cursor-pointer flex justify-center items-center col-start-9 col-span-4 bg-red-900 hover:bg-red-800 text-white font-bold rounded h-3/4">Remove All</div>
-                </div>
+                }
+        
             </div>
-            }
-       
+            <div className="h-2 handle cursor-move"></div>
            </div>
     )
 });
